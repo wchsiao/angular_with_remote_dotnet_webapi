eae0726f61e590729c4f8b27c0c67df31c846839
WH <wch.hsiao@gmail.com>
2023-01-03 10:43:28 -0500

    Added batch to simplify lunch

---
+++ launch.bat
@@ -0,0 +1,2 @@
+start "NG" cmd /c launch_dotnet_webapi.bat
+start "DOTNET" cmd /c launch_angular.bat
---
+++ launch_angular.bat
@@ -0,0 +1,7 @@
+cd .\angular-prj\
+
+@echo [npm install]
+call npm install
+
+@echo [ng serve]
+ng serve --open
