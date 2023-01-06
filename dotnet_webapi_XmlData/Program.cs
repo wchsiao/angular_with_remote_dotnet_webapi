07e0693e584f82b533211f8f9bd818554aaefe5d
Wei-Chung Hsiao <wch.hsiao@gmail.com>
2023-01-05 19:02:32 -0500

    Added cors, and data file

--- dotnet_webapi_XmlData/Program.cs
+++ dotnet_webapi_XmlData/Program.cs
@@ -2,6 +2,15 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("PolicyCors",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200");
+        });
+});
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -15,6 +24,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors();
 
 app.UseHttpsRedirection();
 
