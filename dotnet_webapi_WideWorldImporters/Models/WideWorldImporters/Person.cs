60c704ba633c2de623fe98b1457e31d4b4deb9a7
Wei-Chung Hsiao <wch.hsiao@gmail.com>
2023-01-08 01:57:53 -0500

    Removed project (features replaced by SimpleTest)

--- dotnet_webapi_WideWorldImporters/Models/WideWorldImporters/Person.cs
+++
@@ -1,8 +0,0 @@
-namespace dotnet_webapi_WideWorldImporters.Models.WideWorldImporters;
-
-public class Person
-{
-    public int PersonId { get; set; }
-
-    public string? FullName { get; set; }
-}
